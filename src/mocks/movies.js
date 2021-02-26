e8a1c285ae2ce108a6ec329da063327e22febae2
raulng9 <salprez4@gmail.com>
2021-02-26 19:27:55 +0100

    Added initial files

---
+++ src/mocks/movies.js
@@ -0,0 +1,434 @@
+export const movies = [
+  {
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+  },
+  {
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+  },
+  {
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+  },
+  {
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+  },
+  {
+    Title: "The Avengers",
+    Year: "1998",
+    imdbID: "tt0118661",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+  },
+  {
+    Title: "The Avengers: Earth's Mightiest Heroes",
+    Year: "2010–2012",
+    imdbID: "tt1626038",
+    Type: "series",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
+  },
+  {
+    Title: "Ultimate Avengers: The Movie",
+    Year: "2006",
+    imdbID: "tt0491703",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTYyMjk0NTMwMl5BMl5BanBnXkFtZTgwNzY0NjAwNzE@._V1_SX300.jpg"
+  },
+  {
+    Title: "Ultimate Avengers II",
+    Year: "2006",
+    imdbID: "tt0803093",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+  },
+  {
+    Title: "The Avengers",
+    Year: "1961–1969",
+    imdbID: "tt0054518",
+    Type: "series",
+    Poster: "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+  },
+  {
+    Title: "Avengers Assemble",
+    Year: "2013–2019",
+    imdbID: "tt2455546",
+    Type: "series",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg"
+  }
+]
+
+export const movieDetails = {
+  tt0848228:{
+    "Title": "The Avengers",
+    "Year": "2012",
+    "Rated": "PG-13",
+    "Released": "04 May 2012",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Sci-Fi",
+    "Director": "Joss Whedon",
+    "Writer": "Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)",
+    "Actors": "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+    "Plot": "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    "Language": "English, Russian, Hindi",
+    "Country": "USA",
+    "Awards": "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "8.0/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "91%"
+      },
+      {
+        "Source": "Metacritic",
+        "Value": "69/100"
+      }
+    ],
+    "Metascore": "69",
+    "imdbRating": "8.0",
+    "imdbVotes": "1,263,208",
+    "imdbID": "tt0848228",
+    "Type": "movie",
+    "DVD": "22 Nov 2015",
+    "BoxOffice": "$623,357,910",
+    "Production": "Marvel Studios",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt4154756:{
+    "Title": "Avengers: Infinity War",
+    "Year": "2018",
+    "Rated": "PG-13",
+    "Released": "27 Apr 2018",
+    "Runtime": "149 min",
+    "Genre": "Action, Adventure, Sci-Fi",
+    "Director": "Anthony Russo, Joe Russo",
+    "Writer": "Christopher Markus (screenplay by), Stephen McFeely (screenplay by), Stan Lee (based on the Marvel comics by), Jack Kirby (based on the Marvel comics by), Joe Simon (Captain America created by), Jack Kirby (Captain America created by), Steve Englehart (Star-Lord created by), Steve Gan (Star-Lord created by), Bill Mantlo (Rocket Raccoon created by), Keith Giffen (Rocket Raccoon created by), Jim Starlin (Thanos,  Gamora and Drax created by), Stan Lee (Groot created by), Larry Lieber (Groot created by), Jack Kirby (Groot created by), Steve Englehart (Mantis created by), Don Heck (Mantis created by)",
+    "Actors": "Robert Downey Jr., Chris Hemsworth, Mark Ruffalo, Chris Evans",
+    "Plot": "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "Nominated for 1 Oscar. Another 46 wins & 73 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "8.4/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "85%"
+      },
+      {
+        "Source": "Metacritic",
+        "Value": "68/100"
+      }
+    ],
+    "Metascore": "68",
+    "imdbRating": "8.4",
+    "imdbVotes": "839,788",
+    "imdbID": "tt4154756",
+    "Type": "movie",
+    "DVD": "31 Jul 2018",
+    "BoxOffice": "$678,815,482",
+    "Production": "Marvel Studios",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt4154796:{
+    "Title": "Avengers: Endgame",
+    "Year": "2019",
+    "Rated": "PG-13",
+    "Released": "26 Apr 2019",
+    "Runtime": "181 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Anthony Russo, Joe Russo",
+    "Writer": "Christopher Markus (screenplay by), Stephen McFeely (screenplay by), Stan Lee (based on the Marvel comics by), Jack Kirby (based on the Marvel comics by), Joe Simon (Captain America created by), Jack Kirby (Captain America created by), Steve Englehart (Star-Lord created by), Steve Gan (Star-Lord created by), Bill Mantlo (Rocket Raccoon created by), Keith Giffen (Rocket Raccoon created by), Jim Starlin (Thanos,  Gamora & Drax created by), Stan Lee (Groot created by), Larry Lieber (Groot created by), Jack Kirby (Groot created by), Steve Englehart (Mantis created by), Don Heck (Mantis created by)",
+    "Actors": "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+    "Plot": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+    "Language": "English, Japanese, Xhosa, German",
+    "Country": "USA",
+    "Awards": "Nominated for 1 Oscar. Another 69 wins & 102 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "8.4/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "94%"
+      },
+      {
+        "Source": "Metacritic",
+        "Value": "78/100"
+      }
+    ],
+    "Metascore": "78",
+    "imdbRating": "8.4",
+    "imdbVotes": "816,700",
+    "imdbID": "tt4154796",
+    "Type": "movie",
+    "DVD": "30 Jul 2019",
+    "BoxOffice": "$858,373,000",
+    "Production": "Marvel Studios, Walt Disney Pictures",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt2395427:{
+    "Title": "Avengers: Age of Ultron",
+    "Year": "2015",
+    "Rated": "PG-13",
+    "Released": "01 May 2015",
+    "Runtime": "141 min",
+    "Genre": "Action, Adventure, Sci-Fi",
+    "Director": "Joss Whedon",
+    "Writer": "Joss Whedon, Stan Lee (based on the Marvel comics by), Jack Kirby (based on the Marvel comics by), Joe Simon (character created by: Captain America), Jack Kirby (character created by: Captain America), Jim Starlin (character created by: Thanos)",
+    "Actors": "Robert Downey Jr., Chris Hemsworth, Mark Ruffalo, Chris Evans",
+    "Plot": "When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it's up to Earth's mightiest heroes to stop the villainous Ultron from enacting his terrible plan.",
+    "Language": "English, Korean",
+    "Country": "USA",
+    "Awards": "8 wins & 49 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "7.3/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "75%"
+      },
+      {
+        "Source": "Metacritic",
+        "Value": "66/100"
+      }
+    ],
+    "Metascore": "66",
+    "imdbRating": "7.3",
+    "imdbVotes": "740,541",
+    "imdbID": "tt2395427",
+    "Type": "movie",
+    "DVD": "08 Sep 2015",
+    "BoxOffice": "$459,005,868",
+    "Production": "Marvel Studios",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt0118661:{
+    "Title": "The Avengers",
+    "Year": "1998",
+    "Rated": "PG-13",
+    "Released": "14 Aug 1998",
+    "Runtime": "89 min",
+    "Genre": "Action, Adventure, Sci-Fi, Thriller",
+    "Director": "Jeremiah S. Chechik",
+    "Writer": "Sydney Newman (television series The Avengers), Don MacPherson",
+    "Actors": "Ralph Fiennes, Uma Thurman, Sean Connery, Patrick Macnee",
+    "Plot": "Two British Agents team up to stop Sir August de Wynter from destroying the world with a weather-changing machine.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "4 wins & 17 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "3.8/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "5%"
+      },
+      {
+        "Source": "Metacritic",
+        "Value": "12/100"
+      }
+    ],
+    "Metascore": "12",
+    "imdbRating": "3.8",
+    "imdbVotes": "41,038",
+    "imdbID": "tt0118661",
+    "Type": "movie",
+    "DVD": "01 Sep 2008",
+    "BoxOffice": "$23,384,939",
+    "Production": "Warner Brothers",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt1626038:{
+    "Title": "The Avengers: Earth's Mightiest Heroes",
+    "Year": "2010–2012",
+    "Rated": "TV-Y7",
+    "Released": "22 Sep 2010",
+    "Runtime": "30 min",
+    "Genre": "Animation, Action, Family, Sci-Fi",
+    "Director": "N/A",
+    "Writer": "N/A",
+    "Actors": "Eric Loomis, Colleen O'Shaughnessey, Brian Bloom, Rick D. Wasserman",
+    "Plot": "After 74 villains break out of prison, Marvel's most powerful superheroes team up to capture all of them, and also to defend the Earth from widespread threats.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "8 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "8.3/10"
+      }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "8.3",
+    "imdbVotes": "13,103",
+    "imdbID": "tt1626038",
+    "Type": "series",
+    "totalSeasons": "2",
+    "Response": "True"
+  },
+  tt0491703:{
+    "Title": "Ultimate Avengers: The Movie",
+    "Year": "2006",
+    "Rated": "PG-13",
+    "Released": "21 Feb 2006",
+    "Runtime": "72 min",
+    "Genre": "Animation, Action, Adventure, Sci-Fi",
+    "Director": "Curt Geda, Steven E. Gordon, Bob Richardson",
+    "Writer": "Mark Millar (comic book: \"The Ultimates\"), Bryan Hitch (comic book: \"The Ultimates\"), Greg Johnson (screen story), Boyd Kirkland (screen story), Craig Kyle (screen story), Greg Johnson (screenplay), Christopher L. Yost (additional writing), Joe Simon (comic book & character: Captain America), Jack Kirby (comic book & character: Captain America)",
+    "Actors": "Justin Gross, Grey Griffin, Michael Massee, Marc Worden",
+    "Plot": "To confront an alien menace, General Fury assembles a team of superheroes lead by a recently resuscitated Captain America.",
+    "Language": "Spanish, English",
+    "Country": "USA",
+    "Awards": "1 win & 1 nomination.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTYyMjk0NTMwMl5BMl5BanBnXkFtZTgwNzY0NjAwNzE@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "6.7/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "59%"
+      }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "6.7",
+    "imdbVotes": "11,939",
+    "imdbID": "tt0491703",
+    "Type": "movie",
+    "DVD": "06 Oct 2016",
+    "BoxOffice": "N/A",
+    "Production": "Marvel Studios Inc.",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt0803093:{
+    "Title": "Ultimate Avengers II",
+    "Year": "2006",
+    "Rated": "PG-13",
+    "Released": "08 Aug 2006",
+    "Runtime": "73 min",
+    "Genre": "Animation, Action, Adventure, Sci-Fi",
+    "Director": "Will Meugniot, Dick Sebast, Bob Richardson",
+    "Writer": "Mark Millar (comic book: \"The Ultimates\"), Bryan Hitch (comic book: \"The Ultimates\"), Greg Johnson (screen story), Craig Kyle (screen story), Greg Johnson (screenplay), Joe Simon (comic book & character: Captain America), Jack Kirby (comic book & character: Captain America)",
+    "Actors": "Justin Gross, Grey Griffin, Michael Massee, Marc Worden",
+    "Plot": "When the Chitauri invaders are sighted in the African kingdom of Wakanda, the Avengers covertly enter the advanced nation to investigate.",
+    "Language": "Spanish, English",
+    "Country": "USA",
+    "Awards": "1 nomination.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "6.6/10"
+      },
+      {
+        "Source": "Rotten Tomatoes",
+        "Value": "50%"
+      }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "6.6",
+    "imdbVotes": "8,697",
+    "imdbID": "tt0803093",
+    "Type": "movie",
+    "DVD": "06 Oct 2016",
+    "BoxOffice": "N/A",
+    "Production": "Marvel Enterprises",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  tt0054518:{
+    "Title": "The Avengers",
+    "Year": "1961–1969",
+    "Rated": "TV-14",
+    "Released": "28 Mar 1966",
+    "Runtime": "60 min",
+    "Genre": "Action, Comedy, Crime, Mystery, Romance, Sci-Fi, Thriller",
+    "Director": "N/A",
+    "Writer": "Sydney Newman",
+    "Actors": "Patrick Macnee",
+    "Plot": "A quirky spy show of the adventures of eccentrically suave British Agent John Steed and his predominately female partners.",
+    "Language": "English",
+    "Country": "UK",
+    "Awards": "Nominated for 4 Primetime Emmys. Another 1 win.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "8.3/10"
+      }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "8.3",
+    "imdbVotes": "6,825",
+    "imdbID": "tt0054518",
+    "Type": "series",
+    "totalSeasons": "7",
+    "Response": "True"
+  },
+  tt2455546:{
+    "Title": "Avengers Assemble",
+    "Year": "2013–2019",
+    "Rated": "TV-Y7",
+    "Released": "26 May 2013",
+    "Runtime": "23 min",
+    "Genre": "Animation, Action, Adventure, Sci-Fi",
+    "Director": "N/A",
+    "Writer": "Jack Kirby, Joe Simon, Stan Lee",
+    "Actors": "Roger Craig Smith, Troy Baker, Fred Tatasciore, Travis Willingham",
+    "Plot": "The further adventures of the Marvel Universe's mightiest general membership superhero team.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "11 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg",
+    "Ratings": [
+      {
+        "Source": "Internet Movie Database",
+        "Value": "7.0/10"
+      }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "7.0",
+    "imdbVotes": "6,605",
+    "imdbID": "tt2455546",
+    "Type": "series",
+    "totalSeasons": "5",
+    "Response": "True"
+  }
+}
